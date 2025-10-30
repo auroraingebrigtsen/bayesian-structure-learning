ad4d50a14acb954856425a96a847c0ea3f18f8db
aurora <aurorainge@gmail.com>
2025-10-30 21:51:57 +0100

    downwards close

--- partial_order_approach.py
+++ partial_order_approach.py
@@ -11,22 +11,27 @@ from pygobnilp.gobnilp import read_local_scores
 
 Edge = Tuple[str, str]
 
-def downwards_closed(LS:Dict[str, Dict[FrozenSet[str], float]])-> Dict[str, Dict[FrozenSet[str], float]]:
+def downwards_closed(LS: Dict[str, Dict[FrozenSet[str], float]]
+) -> Dict[str, Dict[FrozenSet[str], float]]:
     """
     The algorithm assumes that the local scores are downwards closed. That means that if a parent set Z has a defined score for v,
     then all subsets of Z also have a defined score for v.
     This function ensures that the local scores satisfy this property by adding -inf scores for missing subsets.
     """
-    downwards_closed_LS = {}
+    closed = {}
     for v, scored_parent_sets in LS.items():
-        all_parent_sets = set()
+        base = dict(scored_parent_sets)
+        all_ps = set()
         for ps in scored_parent_sets.keys():
-            all_parent_sets.update(frozenset(subset) for r in range(len(ps)+1) for subset in combinations(ps, r))
-        for ps in all_parent_sets:
-            if ps not in scored_parent_sets:
-                scored_parent_sets[ps] = float('-inf')
-        downwards_closed_LS[v] = scored_parent_sets
-    return downwards_closed_LS
+            for r in range(len(ps) + 1):
+                for subset in combinations(ps, r):
+                    all_ps.add(frozenset(subset))
+        all_ps.add(frozenset())
+        for ps in all_ps:
+            if ps not in base:
+                base[ps] = float('-inf')
+        closed[v] = base
+    return closed
 
 def make_blocks_and_fronts(
     V: List[str], m: int, p: int
